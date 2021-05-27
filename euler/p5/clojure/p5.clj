5aecd3ed9e26feb7e01c30a940a34dfcb72a942c
Gabriel Luque <gldidonna@gmail.com>
2021-05-26 21:38:08 -0300

    P5 solves in 0.1 msecs

--- euler/p5/clojure/p5.clj
+++ euler/p5/clojure/p5.clj
@@ -1,15 +1,40 @@
 (ns p5
   (:gen-class))
 
-(defn evenly-divisible? [n div]
-  (= 0 (mod n div)))
+(defn not-divisible? [n div]
+  (not= 0 (mod n div)))
 
-(defn evenly-divisible-range? [n]
-  (every? (partial evenly-divisible? n) (range 1 21)))
+(def sieve
+  ((fn step [s]
+     (lazy-seq
+      (cons
+       (first s)
+       (step
+        (filter #(not-divisible? % (first s))
+                (rest s))))))
+   (iterate inc 2)))
 
-(def smallest-multiple
-  (first
-   (filter evenly-divisible-range? (iterate inc 2520))))
+(defn smallest-multiple [k]
+  (let [primes (take-while #(< % k) sieve)
+        result (reduce
+                (fn [acc prime]
+                  (let [exponent (Math/floor (/ (Math/log k)
+                                                (Math/log prime)))]
+                    (* acc (Math/pow prime exponent))))
+                1
+                primes)]
+    (int result)))
 
-(defn -main []
-  (println smallest-multiple))
+(defn -main [arg]
+  (println (smallest-multiple (Integer/parseInt arg))))
+
+(comment
+  
+  (dotimes [_ 3]
+    (time
+     (smallest-multiple 20)))
+  ;; "Elapsed time: 0.168377 msecs"
+  ;; "Elapsed time: 0.047399 msecs"
+  ;; "Elapsed time: 0.070401 msecs"
+
+)
