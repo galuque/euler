82759bd36c9fcc69a448bf78db7425d664c189e3
Gabriel Luque <gldidonna@gmail.com>
2021-05-30 19:00:08 -0300

    Use utils fns

--- clojure/src/galuque/euler/p6.clj
+++ clojure/src/galuque/euler/p6.clj
@@ -1,22 +1,15 @@
 (ns galuque.euler.p6
-  (:gen-class))
+  (:gen-class)
+  (:require [galuque.euler.utils :as u]))
 
 (defn sum-sqr-difference [k]
-  (let [m (-> (reduce (fn [acc n]
-                        (-> acc
-                            (update-in [:sum]  + n)
-                            (update-in [:sqrs] + (* n n))))
-                      {:sum 0
-                       :sqrs 0}
-                      (range 1 (inc k)))
-              (update-in [:sum] #(Math/pow % 2)))]
-    (- (:sum m) (:sqrs m))))
+  (- (u/square-of-sums k)
+     (u/sum-of-squares k)))
 
 (comment
   (sum-sqr-difference 100)
   ;; => 25164150
-)
+  )
+
 (defn -main [arg]
-  (let [answer (sum-sqr-difference (Long/parseLong arg))]
-    (println
-     (format "%d" (biginteger answer)))))
+  (println (sum-sqr-difference (Long/parseLong arg))))
