386bf079a17a1f0eb1bcc7a32284e4b0e2d18b56
Gabriel Luque <gldidonna@gmail.com>
2021-05-30 12:32:54 -0300

    P10 solution

---
+++ clojure/src/galuque/euler/p10.clj
@@ -0,0 +1,18 @@
+(ns galuque.euler.p10
+  (:gen-class)
+  (:require [galuque.euler.utils :as utils]))
+
+(defn primes-under-n [n]
+  (take-while #(< % n) (utils/sieve)))
+
+(comment
+  (time
+   (apply + (primes-under-n 2000000)))
+  ;; "Elapsed time: 1236.391263 msecs"
+  ;; => 142913828922
+)
+
+(defn -main [arg]
+  (let [primes (primes-under-n (Long/parseLong arg))
+        result (apply + primes)]
+    (println result)))
